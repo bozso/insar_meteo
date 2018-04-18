f1c7a34f711cd1fa50420088076c70314d09c36f
Bozsó István <istvan@robosztus.ggki.hu>
2018-04-18 17:47:57 +0200

    Added gmtpy.py from https://github.com/emolch/gmtpy as an inspiration for gmt.py

--- daisy/src/compile.py
+++ daisy/src/compile.py
@@ -2,18 +2,32 @@ from subprocess import check_output, CalledProcessError, STDOUT
 from shlex import split
 from os.path import basename
 
-flags = ""
+def cmd(command, ret_out=True):
+    try:
+        cmd_out = check_output(split(command), stderr=STDOUT)
+    except CalledProcessError as e:
+        print("Command failed, command: '{}'".format(cmd))
+        print("OUTPUT OF THE COMMAND: \n{}".format(e.output.decode()))
+        print("RETURNCODE: \n{}".format(e.returncode))
+    
+    if ret_out:
+        return cmd_out.decode()
 
+
+CC = "gcc"
+flags = "-std=c99 -static " + cmd("gsl-config --cflags --libs")
 inputfile = "daisy.c"
 
 def main():
 
-    cmd = "gcc {} -o {}".format(inputfile, basename(inputfile).split('.')[0])
+    comp_cmd = "{} {} -o {}".format(CC, inputfile,
+                                    basename(inputfile).split('.')[0])
+    comp_cmd += " " + flags
     
     try:
-        cmd_out = check_output(split(cmd + " " + flags), stderr=STDOUT)
+        cmd_out = check_output(split(comp_cmd), stderr=STDOUT)
     except CalledProcessError as e:
-        print("Compilation failed, compiler command: '{}'".format(cmd))
+        print("Compilation failed, compiler command: '{}'".format(comp_cmd))
         print("OUTPUT OF THE COMMAND: \n{}".format(e.output.decode()))
 
 if __name__ == "__main__":
--- daisy/src/daisy.c
+++ daisy/src/daisy.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+
 #include "aux_macros.h"
 
 int main(int argc, char **argv)
 {
     double a[3];
-
+    //println("J0(%g) = %.18e", 5.0, gsl_sf_bessel_J0(5.0));
+    
+    //return 0;
+    
+    /*for(double ii = 0.0; ii < 1e6; ii++) {
+        a[0] = 0.0;
+        a[1] = ii;
+        a[2] = ii * 2;
+        fwrite(a, 3 * sizeof(double), 1, stdout);
+    }*/
+    
     while (fread(a, 3 * sizeof(double), 1, stdin) > 0) {
+        //fwrite(a, 3 * sizeof(double), 1, stdout);
         println("%lf %lf %lf", a[0], a[1], a[2]);
     }
     

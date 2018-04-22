b81bd3e2cd68c93483db9d286015c08504bb9ec2
István Bozsó <bozso.istvan@csfk.mta.hu>
2018-04-22 22:27:57 +0200

    added aux_module.[c|h]

---
+++ aux/aux_module.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void * malloc_or_exit(size_t nbytes, const char * file, int line)
+{	
+    void *x;
+	
+    if ((x = malloc(nbytes)) == NULL) {	
+        error("%s:line %d: malloc() of %zu bytes failed\n",	
+              file, line, nbytes);
+        exit(Err_Alloc);
+    }
+    else
+        return x;
+}
+
+static FILE * sfopen(const char * path, const char * mode)
+{
+    FILE * file = fopen(path, mode);
+
+    if (!file) {
+        error("Error opening file: \"%s\". ", path);
+        perror("fopen");
+        exit(Err_Io);
+    }
+    return file;
+}

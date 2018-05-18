34fad8a6b65c2795ac4d6717dfe913b62fd1d8ff
István Bozsó <bozso.istvan@csfk.mta.hu>
2018-05-18 14:35:52 +0200

    m2html did not generate documentation for subfunctions

--- Matlab/gendoc.m
+++
@@ -1,9 +0,0 @@
-function [] = gendoc()
-% Generate html documentation with m2html.
-%
-    cd ..
-    addpath /home/istvan/progs/m2html;
-    
-    m2html('mfiles', 'Matlab', 'htmldir', 'doc');
-    cd Matlab
-end

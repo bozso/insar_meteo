9c0c7610877eddd1b4415cce71b007a68bc3d468
István Bozsó <bozso.istvan@csfk.mta.hu>
2018-06-13 18:32:28 +0200

    started experimenting with xarray

--- src/compile.py
+++ src/compile.py
@@ -3,14 +3,16 @@ from distutils.ccompiler import new_compiler
 from sys import argv
 
 c_file = [argv[1]]
-libs = ["m"]
-flags = ["-std=c99", "-O3"]
+#libs = ["m"]
+libs=None
+flags = ["-std=c++14"]
+inc_dir = ["/home/istvan/miniconda3/include"]
 
 def main():
     c_basename = c_file[0].split(".")[0]
     
     ccomp = new_compiler()
-    ccomp.compile(c_file, extra_postargs=flags)
+    ccomp.compile(c_file, extra_postargs=flags, include_dirs=inc_dir)
     
     ccomp.link_executable([c_basename + ".o"],
                           pjoin("..", "bin", c_basename),
